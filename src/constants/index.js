866e53cf06bed0502ddb26e02c1fee02f2ae4259
DvaliElgudja <dvali.Elgudja@gmail.com>
2023-11-17 02:02:58 +0100

    .

---
+++ src/constants/index.js
@@ -0,0 +1,218 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'NinaCarducci',
+        description: "As a freelance developer, I have decided to expand my services by offering SEO optimization to new clients. My approach includes a comprehensive analysis of their current website performance, identifying areas for improvement in terms of search engine visibility and user engagement. By implementing targeted strategies such as keyword optimization, content enhancement, and technical SEO adjustments, I aim to boost their online presence and drive more organic traffic to their site. My services also encompass continuous monitoring and adjustments to adapt to the ever-changing algorithms of search engines, ensuring sustained improvements in search rankings and online visibility for my clients.",
+        link: 'https://github.com/DvaliElgudja/NinaCarducci',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Booki',
+        description: "Booki is a web project aimed at simplifying the search for accommodations and activities in the user's chosen city. As a Front-End developer on this project, my role was to convert the mockups provided by the UI designer into a functional reality using HTML and CSS. This involved ensuring that each element of the user interface was accurately rendered and optimized for a smooth and intuitive user experience. My work also included ensuring the site's responsiveness across various devices and screen resolutions, thereby guaranteeing maximum accessibility and efficiency for all users.",
+        link: 'https://github.com/DvaliElgudja/booki',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Kasa',
+        description: "I worked as a Front-End Developer on the Kasa project, which focused on completely revamping the website, transitioning from ASP.NET to a modern JavaScript stack. My primary role was to develop the front-end using React, ensuring the implementation of the designer's mockups into a responsive and user-friendly interface. I also collaborated with the back-end team, which used Node.js, to integrate front-end and back-end operations smoothly. This project sharpened my skills in web development and collaborative teamwork.",
+        link: 'https://github.com/DvaliElgudja/kasa',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Argent Bank',
+        description: "As a Front-End Developer at Argent Bank, my primary role was to develop a user dashboard with an authentication system using React and Redux. This involved migrating an existing HTML/CSS website to a more advanced React-based platform, focusing on creating a user-friendly interface and ensuring secure login functionality.",
+        link: 'https://github.com/DvaliElgudja/ArgentBank-website',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Sophie Bluel',
+        description: "As a Front-End Developer, I played a crucial role in the completion of a new website for an interior architect. My job involved adding interactive features to enhance user engagement and improve functionality. This included integrating a dynamic gallery to display the architect's work, developing a contact form for easy communication with potential clients, and ensuring the website's design was responsive and visually appealing across different devices.",
+        link: 'https://github.com/DvaliElgudja',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Oh My Food!',
+        description: "OhMyFood is a food startup aiming to develop a website listing menus from gourmet restaurants. As a developer, my role was to build the site in accordance with the mockups provided by the UX design team. This involved translating their design vision into a functional and visually appealing website, ensuring a seamless user experience while browsing through various restaurant menus.",
+        link: 'https://github.com/DvaliElgudja/OhMyFood',
+    }
+];
